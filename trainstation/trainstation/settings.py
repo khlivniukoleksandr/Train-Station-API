73d30805ae88c9296d42d2a47cbedda71a6bfb20
Khlivniuk Oleksandr <sasha2056083@gmail.com>
2025-08-06 16:22:08 +0300

    Add README File and documentation

--- trainstation/trainstation/settings.py
+++ trainstation/trainstation/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ib02ud*$72x37n6r0f03s3u$%7lvo@rk+873ma!02ynpueks(4"
+SECRET_KEY = os.environ.get("SECRET_KEY",)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
